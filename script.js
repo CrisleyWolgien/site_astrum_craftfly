043347ff86a151e59c5ecc4767b02237d465a574
crisleyws <crisleyws@gmail.com>
2025-06-04 23:18:08 -0300

    carrosel marketing pronto

--- script.js
+++ script.js
@@ -26,3 +26,34 @@ prevbtn_sites.addEventListener("click", () => {
 });
 
 update_carrosel_sites();
+
+const const_carrosel_marketing = document.getElementById("carrousel_marketing");
+const prevbtn_marketing = document.getElementById("prev_marketing");
+const nextbtn_marketing = document.getElementById("next_marketing");
+const indicador_marketing = document.getElementById(
+  "slide_indicator_marketing"
+);
+
+const total_slides_marketing = const_carrosel_marketing.children.length;
+let current_slide_marketing = 0;
+
+function update_carrosel_marketing() {
+  const offset = -current_slide_marketing * 100;
+  const_carrosel_marketing.style.transform = `translateX(${offset}%)`;
+  indicador_marketing.textContent = `${
+    current_slide_marketing + 1
+  }/${total_slides_marketing}`;
+}
+
+nextbtn_marketing.addEventListener("click", () => {
+  current_slide_marketing = (current_slide_marketing + 1) % total_slides_marketing;
+  update_carrosel_marketing();
+});
+
+prevbtn_marketing.addEventListener("click", () => {
+  current_slide_marketing =
+    (current_slide_marketing - 1 + total_slides_marketing) % total_slides_marketing;
+  update_carrosel_marketing();
+});
+
+update_carrosel_marketing()
